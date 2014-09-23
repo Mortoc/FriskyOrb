e29d4ee4b2f0fc5fd7411be74f3bbff65fe9020f
Mortoc <mortoc@gmail.com>
2014-09-22 20:04:34 -0400

    (amend)

---
+++ Assets/Levels/Editor/Tests/LevelTrack_Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityTest;
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RtInfinity.Levels.Test
+{
+    [TestFixture]
+    [Category("Gameplay")]
+    internal class LevelTrack_Test : IDisposable
+    {
+		private GameObject _fixtureGameObject = new GameObject("TestTrack");
+		private ITrackGenerator _generator = new RtInfinity.Levels.SpaceRace.SpaceRaceTrackGenerator();
+		
+		public void Dispose ()
+		{
+			GameObject.DestroyImmediate (_fixtureGameObject);
+		}
+
+        [Test]
+        public void LevelTracksGenerateTrackSegments()
+        {
+			var track = new LevelTrack(_generator);
+			var segments = track.Generate();
+			Assert.IsNotNull(segments);
+			Assert.IsNotNull(segments.First());
+        }
+    }
+}
---
+++ Assets/Levels/LevelTrack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+namespace RtInfinity.Levels
+{
+    public interface ILevelTrack
+    {
+        /// <summary>
+        /// Strategy object that is responsible for generating the mesh of a level
+        /// </summary>
+        Vector3 GetSurfacePoint(float travelDist, float x);
+        void UpdatePlayerPosition(float travelDist);
+    }
+
+	public class LevelTrack : ILevelTrack
+	{
+		private List<TrackSegment> _segments;
+		private ITrackGenerator _generator;
+
+		public LevelTrack(ITrackGenerator generator)
+		{
+			_generator = generator;
+			_segments = new List<TrackSegment>();
+		}
+
+		public IEnumerable<TrackSegment> Generate()
+		{
+			return _segments;
+		}
+
+		public Vector3 GetSurfacePoint (float travelDist, float x)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdatePlayerPosition (float travelDist)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
